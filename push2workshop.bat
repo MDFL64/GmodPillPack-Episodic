44f9d41f28d24f55069e7687cc141239712e9472
Adam Coggeshall <dameerkat@gmail.com>
2015-03-31 17:30:16 -0600

    Migration Helper...

--- push2workshop.bat
+++ push2workshop.bat
@@ -6,6 +6,6 @@ echo Please Confirm. Is everything ready?
 
 pause
 "../../../bin/gmad" create -folder . -out packaged.gma
-"../../../bin/gmpublish" update -addon packaged.gma -id %WSID%
+"../../../bin/gmpublish" update -addon packaged.gma -id %WSID% -icon icon.jpg
 del packaged.gma
 pause
